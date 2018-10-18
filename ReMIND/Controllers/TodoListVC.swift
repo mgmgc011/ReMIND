26ebe7929096bb8ea987dbdb4d9ce80f29494363
Chingoo <Chingoo@mingus-macbook-pro.local>
2018-10-18 14:35:17 -0500

    commit

---
+++ ReMIND/Controllers/TodoListVC.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  ReMIND
+//
+//  Created by Chingoo on 10/18/18.
+//  Copyright © 2018 Donovan&Chingoo. All rights reserved.
+//
+
+import UIKit
+
+class TodoListVC: UITableViewController {
+
+    let toDoCellID = "toDoItemCell"
+
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(dataFilePath)
+        
+        loadItems()
+        
+    }
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: toDoCellID, for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+    
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
+        return cell
+    }
+    
+    
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Reminder Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    
+}
+
---
+++ ReMIND/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  ReMIND
+//
+//  Created by Chingoo on 10/18/18.
+//  Copyright © 2018 Donovan&Chingoo. All rights reserved.
+//
+
+import Foundation
+
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+    
+}
